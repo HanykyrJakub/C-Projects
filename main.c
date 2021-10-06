98781e7d18bcd3d1ecd2910713bf4bc1d7265f53
HanykyrJakub <90202111+HanykyrJakub@users.noreply.github.com>
2021-10-06 18:20:01 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
  * File:   main.c
  * Author: Počítač
  *
- * Created on 21. ledna 2021, 14:01
+ * Created on 8. února 2021, 19:59
  */
 
 #include <stdio.h>
@@ -17,6 +17,45 @@
 /*
  * 
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+double nalez (){
+ double cislo ;
+
+    printf ("zadej cislo: ");
+    scanf ("%lf", &cislo);
+    return cislo;
+}
+
+void porovnat (double num_a, double num_b){
+    printf("Větší číslo z těch dvou je %lf.\n", (num_a > num_b) ? num_a : num_b);
+}
+int main() {
+    char opacko = "a";
+    double promen_a, promen_b;
+    
+    while (opacko != "n") {
+        printf ("porovnání dvou čísel\n----------------------\n");
+        promen_a = nalez ();
+        promen_b = nalez ();
+        porovnat (promen_a, promen_b);
+        porovnat (14.6, 24.8);
+        
+        printf ("\nChceš znovu porovnat? [a/n] ");
+        
+        do {
+            opacko = (char) getchar();
+        } while (opacko == "\n");
+    }
+    return (EXIT_SUCCESS);
+}
+
+
 int main(int argc, char** argv) {
 
     return (EXIT_SUCCESS);
